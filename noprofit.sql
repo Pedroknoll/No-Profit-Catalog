62117ba46efab99bde3d2640c8b5f449fa89559b
vagrant <vagrant@vagrant.vm>
2018-04-23 21:38:32 +0000

    Add sql script to create the database

---
+++ noprofit.sql
@@ -0,0 +1,11 @@
+-- Pedro H Knoll
+-- Udacity Fullstack Nanodegree
+-- -- ver 0.1 04/23/18 (PostgreSQL)
+--
+-- Database CREATE for Noprofit organizations app
+
+DROP DATABASE IF EXISTS noprofit;
+
+CREATE DATABASE noprofit;
+
+\connect noprofit;
