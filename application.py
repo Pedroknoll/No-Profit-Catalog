043c50a1ca981f202b98bb4dc01ff839dd7d80f6
vagrant <vagrant@vagrant.vm>
2018-04-23 21:35:01 +0000

    Create simple routes to all necessary actions

--- application.py
+++ application.py
@@ -4,9 +4,41 @@ from flask import Flask
 # app configuration
 app = Flask(__name__)
 
+
+# Show homepage
 @app.route('/')
 def index():
   return 'Hello World'
+
+
+# Show organizations list page
+@app.route('/organizations')
+def showOrganizationsList():
+    return 'Show all the organizations'
+
+
+# Show organization detail
+@app.route('/organization/<int:organization_id>')
+def showOrganizationDetail(organization_id):
+    return 'Show organization details'
+
+
+# Add organization
+@app.route('/organization/new')
+def addOrganization():
+    return 'Add organization'
+
+
+# Edit organization
+@app.route('/organization/<int:organization_id>/edit')
+def editOrganization(organization_id):
+    return "Edit organization"
+
+
+# Delete organization
+@app.route('/organization/<int:organization_id>/delete')
+def deleteOrganization(organization_id):
+    return "Delete organization"
 
 
 if __name__ == "__main__":
