998b7211e39cdcf7c0b40b68312c75dd78f7b041
vagrant <vagrant@vagrant.vm>
2018-04-23 21:23:30 +0000

    Add a simple homepage route to the app

---
+++ application.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+from flask import Flask
+
+# app configuration
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return 'Hello World'
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 5000)
